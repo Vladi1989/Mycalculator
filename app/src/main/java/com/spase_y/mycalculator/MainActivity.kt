495b78e7f217bc2f984f429ad0ea6eba0fa17af0
Vladi1989 <bboyvladi1989@mail.ru>
2023-03-15 15:28:27 +0300

    111

--- app/src/main/java/com/spase_y/mycalculator/MainActivity.kt
+++ app/src/main/java/com/spase_y/mycalculator/MainActivity.kt
@@ -7,66 +7,79 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.buttonCalculate)
-        val firstNumber = findViewById<EditText>(R.id.firstNumber)
-        val symbol = findViewById<EditText>(R.id.symbol)
-        val secondNumber = findViewById<EditText>(R.id.secondNumber)
-        val result = findViewById<TextView>(R.id.result)
+        val btnCalculate = findViewById<Button>(R.id.buttonCalculate)
+        val etFirstNumber = findViewById<EditText>(R.id.firstNumber)
+        val etSymbol = findViewById<EditText>(R.id.symbol)
+        val etSecondNumber = findViewById<EditText>(R.id.secondNumber)
+        val tvResult = findViewById<TextView>(R.id.result)
 
-        button.setOnClickListener {
-            if (firstNumber.text.isNotEmpty() && secondNumber.text.isNotEmpty() && symbol.text.isNotEmpty()){
-                when (symbol.text.toString()){
+        btnCalculate.setOnClickListener {
+            if (etFirstNumber.text.isNotEmpty() && etSecondNumber.text.isNotEmpty() && etSymbol.text.isNotEmpty()){
+                when (etSymbol.text.toString()){
                     "+" -> {
-                        result.text = (firstNumber.text.toString().toDouble() + secondNumber.text.toString().toDouble()).toString()
+                        var resultText:Any = (etFirstNumber.text.toString().toDouble() + etSecondNumber.text.toString().toDouble()).toString()
+                        printer(tvResult,resultText)
                     }
                     "-" -> {
-                        result.text = (firstNumber.text.toString().toDouble() - secondNumber.text.toString().toDouble()).toString()
+                        var resultText:Any = (etFirstNumber.text.toString().toDouble() - etSecondNumber.text.toString().toDouble()).toString()
+                        printer(tvResult,resultText)
                     }
                     "*" -> {
-                        result.text = (firstNumber.text.toString().toDouble() * secondNumber.text.toString().toDouble()).toString()
+                        var resultText:Any = (etFirstNumber.text.toString().toDouble() * etSecondNumber.text.toString().toDouble()).toString()
+                        printer(tvResult,resultText)
                     }
                     "/" -> {
-                        if (secondNumber.text.toString().toDouble() != 0.0) {
-                            result.text = "${
-                                (firstNumber.text.toString().toDouble() / secondNumber.text.toString()
+                        if (etSecondNumber.text.toString().toDouble() != 0.0) {
+                            var resultText:Any = "${
+                                (etFirstNumber.text.toString().toDouble() / etSecondNumber.text.toString()
                                     .toDouble())}"
+                            printer(tvResult,resultText)
                         } else {
                             Toast.makeText(applicationContext,"На ноль делить нельзя",Toast.LENGTH_SHORT).show()
-                            secondNumber.error = "Error"
+                            etSecondNumber.error = "Error"
                         }
                     }
+                    "^" -> {
+                        val cor: Double = etFirstNumber.text.toString().toDouble()
+                        val resultText = cor.pow(etSecondNumber.text.toString().toDouble()).toString()
+                        printer(tvResult,resultText)
+
+
+                    }
+
                     else -> {
                         Toast.makeText(applicationContext,
-                            "          Вы ввели ${symbol.text} \nя не знаю такого знака",Toast.LENGTH_SHORT).show()
-                        symbol.error = "Error"
+                            "          Вы ввели ${etSymbol.text} \nя не знаю такого знака",Toast.LENGTH_SHORT).show()
+                        etSymbol.error = "Error"
                     }
                 }
             }
 
             else {
 
-                if (firstNumber.text.isEmpty()) {
-                    firstNumber.error = "Error"
+                if (etFirstNumber.text.isEmpty()) {
+                    etFirstNumber.error = "Error"
                     Toast.makeText(
                         applicationContext,
                         "Первое поле пустое", Toast.LENGTH_SHORT
                     ).show()
                 }
-                if (secondNumber.text.isEmpty()) {
-                    secondNumber.error = "Error"
+                if (etSecondNumber.text.isEmpty()) {
+                    etSecondNumber.error = "Error"
                     Toast.makeText(
                         applicationContext,
                         "Второе поле пустое", Toast.LENGTH_SHORT
                     ).show()
                 }
-                if (symbol.text.isEmpty()) {
-                    symbol.error = "Error"
+                if (etSymbol.text.isEmpty()) {
+                    etSymbol.error = "Error"
                     Toast.makeText(
                         applicationContext,
                         "Вы не ввели знак", Toast.LENGTH_SHORT
@@ -76,3 +89,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 }
+fun printer(result:TextView, resultText:Any){
+    var resultText2 = resultText
+    if (isInt(resultText.toString().toDouble())){
+        resultText2 = resultText.toString().dropLast(2).toInt()
+    }
+    result.text = resultText2.toString()
+}
+
+fun isInt(a:Double):Boolean{
+    val aString = a.toString()
+    val len = aString.length //3
+    val lastS = aString.get(len-1)
+
+    return lastS == '0'
+}
+
+
